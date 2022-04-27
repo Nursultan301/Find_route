40bb1a8dd67f462559b1111437e951276592b2f4
Nursultan301 <abykeevnursultan14@gmail.com>
2022-04-27 15:23:26 +0600

    Updated forms.py

--- trains/forms.py
+++ trains/forms.py
@@ -4,11 +4,18 @@ from trains.models import Train
 
 
 class TrainForm(forms.ModelForm):
-    name = forms.CharField(label='Номер поезда', widget=forms.TextInput(attrs={"class": 'form-control'}))
-    travel_time = forms.CharField(label='Номер поезда', widget=forms.TextInput(attrs={"class": 'form-control'}))
-    form_city = forms.CharField(label='Номер поезда', widget=forms.Select(attrs={"class": 'form-control'}))
-    to_city = forms.CharField(label='Номер поезда', widget=forms.Select(attrs={"class": 'form-control'}))
+    # name = forms.CharField(label='Номер поезда', widget=forms.TextInput(attrs={"class": 'form-control'}))
+    # travel_time = forms.CharField(label='Номер поезда', widget=forms.TextInput(attrs={"class": 'form-control'}))
+    # form_city = forms.CharField(label='Номер поезда', widget=forms.Select(attrs={"class": 'form-control'}))
+    # to_city = forms.CharField(label='Номер поезда', widget=forms.Select(attrs={"class": 'form-control'}))
 
     class Meta:
         model = Train
         fields = '__all__'
+        
+        widgets = {
+            'name': forms.TextInput(attrs={"class": 'form-control'}),
+            'travel_time': forms.NumberInput(attrs={"class": 'form-control'}),
+            'from_city': forms.Select(attrs={"class": 'form-control'}),
+            'to_city': forms.Select(attrs={"class": 'form-control'}),
+        }
