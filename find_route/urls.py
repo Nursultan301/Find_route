0cfaf03c4c9c285c04867d481a5c3138578e639c
Nursultan301 <abykeevnursultan14@gmail.com>
2022-04-29 23:50:30 +0600

    created a function find_routes

--- find_route/urls.py
+++ find_route/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from routes.views import route_home
+from routes.views import route_home, find_routes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cities.urls')),
     path('trains/', include('trains.urls')),
     path('routes/', route_home, name='route'),
+    path('find_routes/', find_routes, name='find_routes'),
 
 ]
--- routes/views.py
+++ routes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 from routes.forms import RouteForm
 
@@ -6,3 +7,15 @@ from routes.forms import RouteForm
 def route_home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {"form": form})
+
+
+def find_routes(request):
+    if request.POST:
+        form = RouteForm(request.POST)
+        a = 1
+        return render(request, 'routes/home.html', {"form": form})
+
+    else:
+        form = RouteForm()
+        messages.error(request, "Нет данных для поиска")
+        return render(request, 'routes/home.html', {"form": form})
