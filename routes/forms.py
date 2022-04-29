32574a511542767d95c20c2882e26ea5984f6309
Nursultan301 <abykeevnursultan14@gmail.com>
2022-04-29 23:21:03 +0600

    Adding select2

--- routes/forms.py
+++ routes/forms.py
@@ -6,12 +6,13 @@ from cities.models import City
 class RouteForm(forms.Form):
     form_city = forms.ModelChoiceField(label='Откуда',
                                         queryset=City.objects.all(),
-                                        widget=forms.Select(attrs={"class": 'form-control'}))
+                                        widget=forms.Select(attrs={"class": 'form-control js-example-basic-single'}))
     to_city = forms.ModelChoiceField(label='Куда',
                                       queryset=City.objects.all(),
-                                      widget=forms.Select(attrs={"class": 'form-control'}))
+                                      widget=forms.Select(attrs={"class": 'form-control js-example-basic-single'}))
     cities = forms.ModelMultipleChoiceField(label='Через города',
                                             queryset=City.objects.all(),
                                             required=True,
-                                            widget=forms.SelectMultiple(attrs={"class": 'form-control'}))
+                                            widget=forms.SelectMultiple(
+                                                attrs={"class": 'form-control js-example-basic-multiple'}))
     travelling_time = forms.CharField(label='Время в пути', widget=forms.NumberInput(attrs={"class": 'form-control'}))
