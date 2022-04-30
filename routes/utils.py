9b60435da6e7ffeba1ad9737b21a1e265955a820
Nursultan301 <abykeevnursultan14@gmail.com>
2022-04-30 23:14:10 +0600

    Updated utils.py

--- routes/utils.py
+++ routes/utils.py
@@ -1,2 +1,35 @@
+from trains.models import Train
+
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == goal:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
+
+
+def get_graph(qs):
+    graph = {}
+    for q in qs:
+        graph.setdefault(q.from_city_id, set())
+        graph[q.from_city_id].add(q.to_city_id)
+    return graph
+
+
 def get_routes(request, form) -> dict:
-    pass
+    context = {'form': form}
+    qs = Train.objects.all()
+    graph = get_graph(qs)
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    travelling_time = data['travelling_time']
+    all_ways = dfs_paths(graph, from_city.id, to_city.id)
+    if not len(list(all_ways)):
+        raise ValueError('Маршурута, удовлетворяющего условиям не существует')
+    return context
