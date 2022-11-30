52d4b7a3087a99faf387eeb2bec390564c4aa765
Nursultan301 <abykeevnursultan14@gmail.com>
2022-11-30 18:45:27 +0600

    Created City from models

--- cities/apps.py
+++ cities/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CitiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cities'
+    verbose_name = 'Населенные пункты'
