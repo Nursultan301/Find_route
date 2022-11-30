574902695a1273afdbc85193af940d062375574a
Nursultan301 <abykeevnursultan14@gmail.com>
2022-11-30 18:45:27 +0600

    Created City from models

--- cities/admin.py
+++ cities/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import City
+
+
+@admin.register(City)
+class AdminCity(admin.ModelAdmin):
+    pass
--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    title = models.CharField("Называние", max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['title']
