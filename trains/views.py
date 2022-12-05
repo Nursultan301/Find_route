0f7cff85a0648e1dc3147549f1b23dd4df4cd052
Nursultan301 <abykeevnursultan14@gmail.com>
2022-12-05 21:35:49 +0600

    created detail and created train

--- trains/views.py
+++ trains/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
+from trains.forms import TrainForm
 from trains.models import Train
 
 
@@ -8,3 +10,15 @@ class TrainListView(ListView):
     model = Train
     paginate_by = 5
     template_name = 'trains/trains.html'
+
+
+class TrainDetailView(DetailView):
+    model = Train
+    template_name = 'trains/detail_train.html'
+
+
+class TrainCreateView(CreateView):
+    model = Train
+    template_name = 'trains/create_train.html'
+    success_url = reverse_lazy('trains:train')
+    form_class = TrainForm
--- travel/urls.py
+++ travel/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('cities.urls', 'cities'))),
-    path('trans/', include(('trains.urls', 'trains')))
+    path('trains/', include(('trains.urls', 'trains')))
 ]
