587d81911a635041a731efeb59dba734cebabd91
Nursultan301 <abykeevnursultan14@gmail.com>
2022-12-05 08:27:02 +0600

    Create models.py

--- trains/admin.py
+++ trains/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from trains.models import Train
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ('title', 'travel_time', 'from_city', 'to_city')
+    search_fields = ('title', 'travel_time')
+    list_filter = ('from_city', 'to_city')
--- travel/settings.py
+++ travel/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'cities.apps.CitiesConfig',
+    'trains.apps.TrainsConfig',
 
 ]
 
