0191c1d3386b90313ec0ddab1283a17403ebfdf3
Nursultan301 <abykeevnursultan14@gmail.com>
2022-12-06 19:28:47 +0600

    Add function update and create train

--- trains/urls.py
+++ trains/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from trains.views import TrainListView, TrainDetailView, TrainCreateView
+from trains.views import TrainListView, TrainDetailView, TrainCreateView, TrainUpdateView, TrainDeleteView
 
 urlpatterns = [
     path('', TrainListView.as_view(), name='train'),
     path('detail/<int:pk>/', TrainDetailView.as_view(), name='train_detail'),
+    path('update/<int:pk>/', TrainUpdateView.as_view(), name='train_update'),
+    path('delete/<int:pk>/', TrainDeleteView.as_view(), name='train_delete'),
     path('add/', TrainCreateView.as_view(), name='train_create'),
 ]
